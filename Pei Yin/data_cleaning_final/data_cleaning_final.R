4f962519111b7cb992ae4ae91f9012c0a8ee9296
pyli4321 <pyli4321@gmail.com>
2023-06-08 18:44:39 -0400

    test

--- Pei Yin/data_cleaning_final/data_cleaning_final.R
+++ Pei Yin/data_cleaning_final/data_cleaning_final.R
@@ -10,7 +10,8 @@ library(taxize)
 
 #### call your data  ####
 
-data <- read.csv('./data_cleaning_final/soil_sp_database_Pei_Yin.csv', sep=',',header = T, dec = '.')
+data <- read.csv('./Pei Yin/data_cleaning_final/soil_sp_database_Pei_Yin.csv', sep=',', header = T, dec = '.', fileEncoding = "UTF-8")
+data <- read.csv('./Pei Yin/data_cleaning_final/soil_sp_database_Pei_Yin.csv', sep=',', header = T, dec = '.')
 
 
 #### decimals ####
