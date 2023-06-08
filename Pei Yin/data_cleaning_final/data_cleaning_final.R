d493c40a4f5f4730003e8229d0e0c44557bbc279
pyli4321 <pyli4321@gmail.com>
2023-06-08 17:12:05 -0400

    adding lines 1 to 11

---
+++ Pei Yin/data_cleaning_final/data_cleaning_final.R
@@ -0,0 +1,13 @@
+# DATA CLEANING
+
+## LIBRARY
+
+library(dplyr)
+library(stringr)
+library(taxize)
+
+
+#### call your data  ####
+data <- read.csv('./Pei yin/soil_sp_database_final_Pei_Yin.csv', sep=',',header = T, dec = '.')
+
+
