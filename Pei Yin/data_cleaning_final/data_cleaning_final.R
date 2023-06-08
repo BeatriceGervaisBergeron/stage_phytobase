add902c8f62d39b5bdf81cd7826241f7626296b2
pyli4321 <pyli4321@gmail.com>
2023-06-08 17:50:48 -0400

    add a few lines of code from data_cleaning_test

--- Pei Yin/data_cleaning_final/data_cleaning_final.R
+++ Pei Yin/data_cleaning_final/data_cleaning_final.R
@@ -9,6 +9,19 @@ library(taxize)
 
 
 #### call your data  ####
-data <- read.csv('./Pei yin/soil_sp_database_final_Pei_Yin.csv', sep=',',header = T, dec = '.')
+data <- read.csv('./Pei yin/soil_sp_database_Pei_Yin.csv', sep=',',header = T, dec = '.')
+
+
+
+#### decimals ####
+# make sure there is not comma instead of points
+# already with points
+
+# make sure all column are in the correct forms (character or numerical)
+str(data)
+
+
+
+
 
 
