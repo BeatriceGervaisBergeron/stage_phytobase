249dc72e74b931f741e32f475677cf8ca80181a4
pyli4321 <pyli4321@gmail.com>
2023-06-14 12:09:16 -0400

    standardize te_ba units

--- Pei Yin/data_cleaning_final/data_cleaning_final.R
+++ Pei Yin/data_cleaning_final/data_cleaning_final.R
@@ -281,8 +281,13 @@ unique(units$units_b) # "g m-2"   "g"   ""    "g/plant"  " t ha-1 yr-1" "kg/ha" 
 # since biomass is measure really differently, for the moment we'll let it like that
 # 'on hold', to be unify if needed further on
 
-#"units_te_ba" 
-unique(units$units_te_ba)
+#"units_te_ba"  ***A CONTINUER***
+unique(units$units_te_ba) # "mg kg−1" "mg Kg-1"
+# conversion of only mg kg−1 
+data_std <- data_std %>%
+  mutate(units_te_ba = ifelse(units_te_ba == 'mg Kg-1' , 'mg kg−1', units_te_ba)) # replace all mg Kg-1 to mg kg−1
+# verify
+unique(data_std$units_te_ba) # only "mg kg−1" "" 
 
 
 
