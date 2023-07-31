d2c4f036defaa3331d38185434195870420ed196
Amelie21-01 <amelie.guesdon@umontreal.ca>
2023-07-31 12:37:55 -0400

    analyses

--- Amélie/analyses_amelie.R
+++ Amélie/analyses_amelie.R
@@ -11,6 +11,9 @@ library(ggplot2)
 library(lmerTest)
 library(vegan)
 library(RColorBrewer)
+library(MuMIn)
+library(corrplot)
+library(pracma)
 
 #### Import data ####
 data <- read.csv('./Amélie/data_cleaning_final/data_std_cu_cleaned.csv', sep="\t", header = TRUE, dec = '.')
@@ -147,6 +150,8 @@ data <- data %>%
     , n_te_ba_3 = as.numeric(n_te_ba_3)
     , organs_ba_3 = as.factor(organs_ba_3)
   )
+
+
 ##Standardisation
 numeric_columns <- sapply(data, is.numeric)
 data[numeric_columns] <- scale(data[numeric_columns])
@@ -159,14 +164,76 @@ ggplot(data, aes(x = cu_s, y = cu_ba)) +
   geom_point() +
   labs(x = "Cuivre dans le sol (cu_s)", y = "Cuivre dans les plantes (cu_ba)",
        title = "Relation entre les concentrations de cuivre dans les parties aériennes des plantes et dans le sol") +
-  scale_x_continuous(limits = c(0, 10)) +   # Définir les limites de l'axe des x
+  scale_x_continuous(limits = c(0, 10)) +   
   scale_y_continuous(limits = c(0, 5)) 
 
+model <- lm(cu_s ~ P, data = data[numeric_columns])
+model
+summary(model)
+ggplot(data, aes(x = P, y = cu_s)) +
+  geom_point() +
+  labs(x = "Phosphore dans le sol (P)", y = "Cuivre dans le sol (cu_s)",
+       title = "Relation entre les concentrations de phosphore et de cuivre dans le sol")
+
+
+# Définir le seuil pour considérer une plante comme un hyperaccumulateur
+seuil_hyperaccumulator <- 300
+# Ajouter une nouvelle colonne 'Hyperaccumulator' dans data
+data_cu <- data %>%
+  mutate(Hyperaccumulator = ifelse(cu_ba > seuil_hyperaccumulator | cu_ba_1 > seuil_hyperaccumulator | cu_ba_2 > seuil_hyperaccumulator | cu_ba_3 > seuil_hyperaccumulator, "Oui", "Non"))
+# Visualiser les hyperaccumulatrices
+hyperaccumulators <- data_cu %>% filter(Hyperaccumulator == "Oui")
+hyperaccumulators$AccSpeciesName_cor
+
+
+# Filtrer les organes que vous souhaitez inclure dans le boxplot
+organes_inclus <- c("leaves", "shoots", "stems")
+data_filtre <- data %>%
+  filter(organs_ba %in% organes_inclus | 
+           organs_ba_1 %in% organes_inclus | 
+           organs_ba_2 %in% organes_inclus | 
+           organs_ba_3 %in% organes_inclus)
+p <- ggplot(data_filtre, aes(x = organs_ba, organs_ba_1, organs_ba_2, organs_ba_3, y = cu_ba, cu_ba_1, cu_ba_2, cu_ba_3)) +
+  geom_boxplot() +
+  labs(x = "Organes", y = "Concentration de cuivre (mg/kg)",
+       title = "Comparaison des concentrations de cuivre dans les différentes parties aériennes") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 250)) # Ajuster les limites des axes x et y
+# Afficher le graphique
+print(p)
+
+
+# Filtrer les organes que vous souhaitez inclure dans le boxplot
+organes_inclus <- c("leaves", "roots")
+# Filtrer les données pour inclure uniquement les feuilles et les racines
+data_filtre <- data %>%
+  filter(organs_ba %in% organes_inclus | 
+           organs_ba_1 %in% organes_inclus | 
+           organs_ba_2 %in% organes_inclus | 
+           organs_ba_3 %in% organes_inclus | 
+           organs_br %in% organes_inclus)
+# Créer une nouvelle colonne "Organe" indiquant si c'est une feuille ou une racine
+data_filtre <- data_filtre %>%
+  mutate(Organe = ifelse(organs_ba %in% c("leaves"), "Feuilles", "Racines"))
+# Créer le graphique boxplot
+p <- ggplot(data_filtre, aes(x = Organe, y = cu_ba)) +
+  geom_boxplot() +
+  labs(x = "Organe", y = "Concentration de cuivre (mg/kg)",
+       title = "Comparaison des concentrations de cuivre dans les racines et les feuilles") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 250)) # Ajuster les limites de l'axe y
+# Afficher le graphique
+print(p)
+
+
+
+
+
+
 #RDA
-# Extraire les variables environnementales et les concentrations de métaux dans le sol et les plantes
 env_vars <- data[numeric_columns][, c("ph", "clay", "sand", "om", "oc", "N", "P", "map", "mat", "ec", "cec")]
-metal_conc_soil <- data[, c("as_s", "cd_s", "cu_s", "pb_s", "zn_s", "ni_s", "se_s", "co_s", "mn_s", "cr_s", "hg_s")] 
-metal_conc_plants <- data[, c("as_ba", "cd_ba", "cu_ba", "pb_ba", "zn_ba", "ni_ba", "se_ba", "co_ba", "mn_ba", "cr_ba", "hg_ba")]
+metal_conc_soil <- data[numeric_columns][, c("as_s", "cd_s", "cu_s", "pb_s", "zn_s", "ni_s", "se_s", "co_s", "mn_s", "cr_s", "hg_s")] 
+metal_conc_plants <- data[numeric_columns][, c("as_ba", "cd_ba", "cu_ba", "pb_ba", "zn_ba", "ni_ba", "se_ba", "co_ba", "mn_ba", "cr_ba", "hg_ba")]
 metal_conc_plants_imputed <- apply(metal_conc_plants, 2, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x))
 metal_conc_plants_imputed <- as.data.frame(metal_conc_plants_imputed)
 data_complete <- cbind(env_vars, metal_conc_plants_imputed)
@@ -177,79 +244,41 @@ summary(rda_result)
 #PCA
 pca_model <- prcomp(data[numeric_columns][, c("as_s", "cu_s", "pb_s", "zn_s", "se_s", "ni_s", "mn_s", "co", "cr_s", "hg_s", "cd_s")])
 print(summary(pca_model))
+
 #ANOVA
 anova_model <- aov(cu_ba ~ zone_geographique, data = data)
 summary(anova_model)
+
+
+
+
+
+
+
 
 
 ## join the traits to your data 
 traits <- readRDS("./complete_data.rds")
 data_std <- left_join(data , traits, by=c('AccSpeciesName_cor' = 'sp'))
 
-# matrix of useful variables
-# modified all character variable into factorial
-data_std <- data_std %>%
-  mutate(
-    AccSpeciesName_cor = as.factor(AccSpeciesName_cor)
-    , covidence = as.factor(covidence)
-    , author = as.factor(author)
-    , country = as.factor(country)
-    , location = as.factor(location)
-    , latitude = as.factor(latitude)
-    , longitude = as.factor(longitude)
-    , climate = as.factor(climate)
-    , map_units = as.factor(map_units)
-    , soil = as.factor(soil)
-    , type = as.factor(type)
-    , om_units = as.factor(om_units)
-    , oc_units = as.factor(oc_units)
-    , texture = as.factor(texture)
-    , clay_units = as.factor(clay_units)
-    , ec_units = as.factor(ec_units)
-    , sand_units = as.factor(sand_units)
-    , cec_units = as.factor(cec_units)
-    , N_units = as.factor(N_units)
-    , N_types = as.factor(N_types)
-    , P_units = as.factor(P_units)
-    , P_type = as.factor(P_type)
-    , units_s = as.factor(units_s)
-    , fraction_s = as.factor(fraction_s)
-    , Bioavailable = as.factor(Bioavailable)
-    , sp = as.factor(sp)
-    , name = as.factor(name)
-    , cultivar = as.factor(cultivar)
-    , units_density = as.factor(units_density)
-    , units_b = as.factor(units_b)
-    , units_te_ba = as.factor(units_te_ba)
-    , organs_ba = as.factor(organs_ba)
-    , units_te_br = as.factor(units_te_br)
-    , organs_br = as.factor(organs_br)
-    , units_te_ba_1 = as.factor(units_te_ba_1)
-    , organs_ba_1 = as.factor(organs_ba_1)
-    , units_te_ba_2 = as.factor(units_te_ba_2)
-    , organs_ba_2 = as.factor(organs_ba_2)
-    , units_te_ba_3 = as.factor(units_te_ba_3)
-    , organs_ba_3 = as.factor(organs_ba_3)
-  )
-
 # remove the sp. that don't have LA, SLA and LDMC values 
 # i.e. remove the lines in which LA values are 'NA'
 data_std <- data_std %>%
   filter(!is.na(LA) & !is.na(SLA) & !is.na(LDMC))
 
-
 # if need no NA
 data_clean <-data_std[,c('as_ba',	'cd_ba',	'cu_ba',	'pb_ba',	'zn_ba',	'se_ba',	'ni_ba',	'co_ba',	'mn_ba',	'cr_ba',	'hg_ba', 'LA' , 'SLA' ,'LDMC' , 'ph' ,'AccSpeciesName_cor', 'covidence')]
-data_clean <- na.omit(data_std[,c('cd_ba','zn_ba','LA' , 'SLA' ,'LDMC' , 'ph' ,'AccSpeciesName_cor', 'covidence')])
+data_clean <- na.omit(data_std[,c('cu_ba', 'cd_ba','zn_ba','LA' , 'SLA' ,'LDMC' , 'ph' ,'AccSpeciesName_cor', 'covidence')])
+
 ## Cd and Zn are the most abundant metal tested (without NA)
-TE_leaves <- data_clean[,c(	'cd_ba',	'zn_ba')]
+TE_leaves <- data_clean[,c(	'cd_ba',	'zn_ba', "cu_ba")]
 
 # TE scores
 ### 1- score of TE accumulation
 # make a matrix only of TE concentrations in leaves
 TE_leaves <- data_std %>%
   filter(organs_ba == "leaves") %>%
-  select(cd_ba, zn_ba)
+  select(cd_ba, zn_ba, cu_ba)
 
 # make a score of all TE
 TE_leaves_std <-decostand(TE_leaves, method='standardize', na.rm = T)
@@ -288,20 +317,35 @@ text(rdap, display='cn', scaling=2, col='black', cex=1, lwd=3)
 label <- rep(c('mn','cu','zn','as','se','cd','ba','pb'), 6)
 #legend('topright', legend =  c('PE','PS','TF','BCFt','BCFr','DC'), col=c(4,1,5,2,0,0), cex = 1 ,pch = c(15,17,18,16,1,25)) # to be adjust with TE
 text(1.7,-0.95, bquote(R^2 == .(round(RsquareAdj(rda)$adj.r.squared, 2))), cex=0.9, pos=2)
-text(1.7,-0.8, bquote(F == .(round(anova(rda, permutation=how(nperm=99999, block=data.std$covidence))$F[1], 2))), cex=0.9, pos=2)
+text(1.7,-0.8, bquote(F == .(round(anova(rda, permutation=how(nperm=99999, block=data_std$covidence))$F[1], 2))), cex=0.9, pos=2)
 
 dev.off()
 
 
 #### Influence of traits and environmental factor on TE accumulation- Linear mix model (LMM) ####
 # Example of a full model selection
-lmer<-lmer(cu_ba ~ LA + SLA + LDMC + ph + country + AccSpeciesName_cor + (1|covidence), data=data_std)
+#Spécifiez les noms des variables numériques à standardiser
+vars_to_standardize <- c("LA","SLA","LDMC","ph","cu_s")
+# Standardisation des variables
+data_std[vars_to_standardize] <- scale(data_std[vars_to_standardize])
+
+
+lmer<-lmer(cu_ba ~ om + cu_s+ LA + SLA + LDMC + ph + (1|covidence), data=data_std)
 anova(lmer)
+
+lmer<-lmer(cu_s ~ om + oc + ec + cec + ph + (1|covidence), data=data_std)
+anova(lmer)
+
+
 # Assumptions verification
 plot(resid(lmer),data_std$cu_ba )
 plot(lmer)
 qqmath(lmer, id=0.05)
 shapiro.test(resid(lmer))
+
+
+
+
 
 # model selection 
 # test different models and interaction
@@ -357,8 +401,6 @@ hist(logit(data_cu$cu_ba))
 hist(sqrt(data_cu$cu_ba))
 hist(data_cu$cu_ba^(1/3))
 
-# cu only database
-data_cu <- data_std %>% filter(!is.na(cu_s))
 # check normality of cu_s
 par(mfrow = c(2,3))
 hist(data_cu$cu_s)
